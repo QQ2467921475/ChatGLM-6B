bc71373ce51878398832e7eee61b1e1173f6efae
chumanrui <2467921475@qq.com>
2023-04-20 15:38:52 +0800

    修改运行信息

--- web_demo.py
+++ web_demo.py
@@ -99,4 +99,4 @@ with gr.Blocks() as demo:
     emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
 
 # demo.queue().launch(share=False, inbrowser=True)
-demo.queue().launch(share=True, inbrowser=True,server_name="0.0.0.0", server_port=8000)
+demo.queue().launch(share=True, inbrowser=True, server_port=8000)
